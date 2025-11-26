b9683d938d8d00cebaa35fb87242f18686011d79
manuganuti <manoellamdmoura@gmail.com>
2025-11-26 20:23:51 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,1 @@
-// Alternar Modo Escuro
-const toggle = document.getElementById('dark-toggle');
 
-toggle.addEventListener('click', () => {
-    document.body.classList.toggle('dark');
-
-    // Trocar ícone conforme o modo ativo
-    if (document.body.classList.contains('dark')) {
-        toggle.innerHTML = '<i class="fa-solid fa-sun"></i>';
-    } else {
-        toggle.innerHTML = '<i class="fa-solid fa-moon"></i>';
-    }
-});
